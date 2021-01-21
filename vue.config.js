73d10bfa83fe3d32b56a3502629a7ca9ab2b46a9
DESKTOP-RI6SUG5\George <fgtyu2001@gmail.com>
2021-01-22 00:30:58 +0800

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  devServer: {
-    disableHostCheck: true,
-  },
-  transpileDependencies: ['vuetify'],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Mountainriver/'
+    : '/'
 }
