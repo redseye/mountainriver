8e6f17d0dfd420a0a3a305be1d8f9a693d8d56f4
DESKTOP-RI6SUG5\George <fgtyu2001@gmail.com>
2021-01-22 02:51:16 +0800

    deploy

---
+++ js/chunk-2d0b306f.0ba0297d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0b306f"],{"272f":function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("v-container",{attrs:{id:"about",tag:"section"}},[t("base-subheading",[n._v("了解山川匯")]),t("p",[n._v(" 請打很多自己的自我介紹~~~~~~~~~!要放底圖也可以 ")])],1)},o=[],u={name:"HomeAbout"},s=u,c=t("2877"),i=t("6544"),r=t.n(i),l=t("a523"),b=Object(c["a"])(s,a,o,!1,null,null,null);e["default"]=b.exports;r()(b,{VContainer:l["a"]})}}]);
+//# sourceMappingURL=chunk-2d0b306f.0ba0297d.js.map
---
+++ js/chunk-2d0f0100.6af9aaec.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0100"],{"9b89":function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("section",{attrs:{id:"feed"}},[t("feed",[e._t("default")],2)],1)},c=[],d=(t("d3b7"),{name:"HomeAbout",components:{Feed:function(){return Promise.all([t.e("chunk-4671f6ee"),t.e("chunk-fd7c14dc")]).then(t.bind(null,"f5bd"))}}}),l=d,o=t("2877"),f=Object(o["a"])(l,u,c,!1,null,null,null);n["default"]=f.exports}}]);
+//# sourceMappingURL=chunk-2d0f0100.6af9aaec.js.map
